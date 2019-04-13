c5aebc451fe89d71e3f3dfde85f6b8b534d8348b
Jeronymoo <47647275+Jeronymoo@users.noreply.github.com>
2019-04-13 17:57:41 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+document.getElementById('add').addEventListener('click', function() {
+
+  //Reverse String
+    let primer = document.getElementById('item').value.toUpperCase();
+    function reverseString(str){
+    let revSrring = "";
+    str.split("").forEach(char => revSrring = char + revSrring);
+    return revSrring;}
+    
+    //Replace String    
+    let reversed = reverseString(primer);
+    let mapObj = {
+       A:"T",
+       T:"A",
+       C:"G",
+       G:"C" 
+    };
+    reversed = reversed.replace(/A|T|C|G/gi, function(matched){
+      return mapObj[matched];
+    });
+
+    //Add a li
+    let list = document.getElementById('todo');
+    let item = document.createElement('h4');
+    item.appendChild(document.createTextNode(reversed));
+    list.appendChild(item);
+    
+});
